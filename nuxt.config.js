c0d72fb283761397f1acd42536a1d1d640ec2f25
Amir Hossein Abdolkhalegh <ah.abdolkhalegh@gmail.com>
2019-03-07 23:40:42 +0330

    add logo icons

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,6 +6,33 @@ export default {
         rel: "stylesheet",
         href: "https://fonts.googleapis.com/css?family=Bad+Script|Handlee|Roboto:100,300,400,500,700"
       },
+      {rel: "icon", sizes: "16x16", href: "/icons/favicon-16x16.png"},
+      {rel: "icon", sizes: "32x32", href: "/icons/favicon-32x32.png"},
+      {rel: "icon", sizes: "96x96", href: "/icons/favicon-96x96.png"},
+      {rel: "icon", sizes: "152x152", href: "/icons/favicon-152x152.png"},
+      {rel: "icon", sizes: "160x160", href: "/icons/favicon-160x160.png"},
+      {rel: "icon", sizes: "192x192", href: "/icons/favicon-192x192.png"},
+      {rel: "icon", sizes: "196x196", href: "/icons/favicon-196x196.png"},
+      {rel: "apple-touch-icon", sizes: "180x180", href: "/icons/apple-touch-icon-180x180.png"},
+      {rel: "apple-touch-icon", sizes: "120x120", href: "/icons/apple-touch-icon-120x120.png"},
+      {rel: "apple-touch-icon", sizes: "167x167", href: "/icons/apple-touch-icon-167x167.png"},
+      {rel: "apple-touch-icon", sizes: "152x152", href: "/icons/apple-touch-icon-152x152.png"},
+      {rel: "apple-touch-icon", sizes: "114x114", href: "/icons/apple-touch-icon-114x114.png"},
+      {rel: "apple-touch-icon", sizes: "144x144", href: "/icons/apple-touch-icon-144x144.png"},
+      {rel: "apple-touch-icon", sizes: "72x72", href: "/icons/apple-touch-icon-72x72.png"},
+      {rel: "apple-touch-icon", sizes: "57x57", href: "/icons/apple-touch-icon-57x57.png"},
+      {rel: "apple-touch-icon", sizes: "80x80", href: "/icons/apple-touch-icon-80x80.png"},
+      {rel: "apple-touch-icon", sizes: "87x87", href: "/icons/apple-touch-icon-87x87.png"},
+      {rel: "apple-touch-icon", sizes: "58x58", href: "/icons/apple-touch-icon-58x58.png"},
+      {rel: "apple-touch-icon", sizes: "60x60", href: "/icons/apple-touch-icon-60x60.png"},
+      {rel: "apple-touch-icon", sizes: "64x64", href: "/icons/apple-touch-icon-64x64.png"},
+      {rel: "apple-touch-icon", sizes: "40x40", href: "/icons/apple-touch-icon-40x40.png"},
+      {rel: "icon", sizes: "48x48", href: "/icons/windows-device-48x48.png"},
+      {rel: "icon", sizes: "62x62", href: "/icons/windows-device-62x62.png"},
+      {rel: "icon", sizes: "173x173", href: "/icons/windows-device-173x173.png"},
+      {rel: "icon", sizes: "99x99", href: "/icons/windows-device-99x99.png"},
+      {rel: "icon", sizes: "200x200", href: "/icons/windows-device-200x200.png"},
+      {rel: "icon", sizes: "256x256", href: "/icons/windows-device-256x256.png"}
     ],
     htmlAttrs: {
       lang: 'en'
@@ -20,6 +47,9 @@ export default {
   },
   plugins: [
     '~plugins/fontawesome.js'
+  ],
+  modules: [
+    '@nuxtjs/axios',
   ],
   build: {
     extractCSS: true,
--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,42 @@
 <template>
-    <header class="hero is-fullheight is-primary">
-        <div class="hero-head">
-            <nav class="navbar">
-                <div class="navbar-brand">
-                    <div class="navbar-item has-text-weight-bold is-size-4">
-                        Taskify
-                    </div>
-                </div>
-            </nav>
+    <div>
+        <div class="logo-background-container">
+            <v-logo class="logo-background"></v-logo>
         </div>
-        <div class="hero-body has-text-centered">
-            <div class="container">
-                <div class="title">
-                    Taskify Your Works
-                </div>
-                <div class="subtitle has-text-white-ter">
-                    Create a project, organize your tasks into a Kanban board, integrate them with GitHub issues
-                </div>
-                <button class="button is-warning is-medium">
+        <header class="hero is-fullheight is-primary">
+            <div class="hero-head">
+                <nav class="navbar">
+                    <div class="navbar-brand">
+                        <div class="navbar-item">
+                            <v-logo class="icon is-medium logo"></v-logo>
+                            <span class="has-text-weight-bold is-size-4">Taskify</span>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+            <div class="hero-body has-text-centered">
+                <div class="container">
+                    <div class="title">
+                        Taskify Your Works
+                    </div>
+                    <div class="subtitle has-text-white-ter">
+                        Create a project, organize your tasks into a Kanban board, integrate them with GitHub issues
+                    </div>
+                    <button class="button is-warning is-medium" @click="signUpWithGithub">
                     <span class="icon">
                         <font-awesome-icon :icon="icons.github"></font-awesome-icon>
                     </span>
-                    <span>Sign up with GitHub</span>
-                </button>
+                        <span>Sign up with GitHub</span>
+                    </button>
+                </div>
             </div>
-        </div>
-    </header>
+        </header>
+    </div>
 </template>
 
 <script>
     import {faGithub} from "@fortawesome/free-brands-svg-icons"
+    import vLogo from '@/components/v-logo.vue'
 
     export default {
         head() {
@@ -37,17 +44,57 @@
                 title: "Home"
             }
         },
+        components: {
+            vLogo
+        },
         data() {
             return {
                 icons: {
                     github: faGithub,
                 }
             }
-        }
+        },
+        methods: {
+            signUpWithGithub() {
 
+            }
+        },
     }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+    .title, .subtitle {
+        text-shadow: .2rem .2rem 1.4rem rgba(30,30,30,.5) ;
+    }
 
+    .logo {
+        margin: 5px;
+    }
+
+    .logo-background-container {
+        position: absolute;
+        height: 100vh;
+        width: 100vw;
+        top: 0;
+        left: 0;
+        overflow: hidden;
+
+        .logo-background {
+            @media screen and (max-width: 768px) {
+                height: calc(120vh);
+                top: -10vh;
+                left: -35vw;
+            }
+            @media screen and (min-width: 769px) {
+                height: calc(160vh);
+                top: -30vh;
+                left: -10vw;
+            }
+            position: relative;
+
+            g {
+                fill: hsla(0, 0%, 100%, 0.06);
+            }
+        }
+    }
 </style>
